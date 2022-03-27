9091990f9be34683ef7c78ead14ab27082238349
Meredith Nolin <meredith.nolin9@etu.univ-lorraine.fr>
2022-03-27 19:57:36 +0200

    app update

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
+import React from 'react';
+import { Platform, UIManager } from 'react-native';
+import { Provider } from 'react-redux';
+import { initDb } from './src/helper/db';
+import { MainNavigator } from './src/navigation/Navigator';
+import buildStore from './src/store/store';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Test GIT</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const fetchFonts = async () => {
+	await Font.loadAsync({
+		'comfortaa': require('./src/assets/fonts/Comfortaa-VariableFont_wght.ttf'),
+	})
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+if (
+	Platform.OS === "android" &&
+	UIManager.setLayoutAnimationEnabledExperimental
+) {
+	UIManager.setLayoutAnimationEnabledExperimental(true);
+}
+
+const store = buildStore();
+
+initDb().then(() => console.log('DB initialized')).catch(e => console.error(`Failed to initialize DB: ${e}`));
+
+
+export default function App() {
+	const [isFontsLoaded, setFontsLoaded] = React.useState(false);
+	
+	if (!isFontsLoaded) {
+		return (
+			<AppLoading startAsync={fetchFonts}
+				onFinish={() => setFontsLoaded(true)}
+				onError={() => console.log('error')} />
+		)
+	}
+	return (
+		<Provider store={store}>
+			<NavigationContainer>
+				<MainNavigator />
+			</NavigationContainer>
+		</Provider>
+	);
+}
